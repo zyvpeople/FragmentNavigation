b5ed4e6f58045b7918091bc8e66f07ad113c4025
Yaroslav Zozulia <zyvpeople@gmail.com>
2017-01-04 13:23:27 +0200

    removed transaction

--- app/src/main/java/com/develop/zuzik/fragmentnavigation/transaction/action/RemoveTransactionAction.kt
+++
@@ -1,7 +0,0 @@
-package com.develop.zuzik.fragmentnavigation.transaction.action
-
-/**
- * User: zuzik
- * Date: 1/1/17
- */
-class RemoveTransactionAction(val tag: String) : TransactionAction
--- app/src/main/java/com/develop/zuzik/fragmentnavigation/transaction/action/TransactionAction.kt
+++
@@ -1,7 +0,0 @@
-package com.develop.zuzik.fragmentnavigation.transaction.action
-
-/**
- * User: zuzik
- * Date: 1/1/17
- */
-interface TransactionAction
