54a0a225d597c7be76780d8f07250469db65b479
Yaroslav Zozulia <zyvpeople@gmail.com>
2017-01-21 12:20:36 +0200

    created NodeTest

---
+++ fragmentNavigation/src/main/java/com/develop/zuzik/fragmentnavigation/model/fragment/ModelRootNavigationFragment.kt
@@ -0,0 +1,10 @@
+package com.develop.zuzik.fragmentnavigation.model.fragment
+
+import android.support.v4.app.Fragment
+
+/**
+ * User: zuzik
+ * Date: 1/21/17
+ */
+class ModelRootNavigationFragment : Fragment() {
+}
---
+++ fragmentNavigation/src/test/java/com/develop/zuzik/fragmentnavigation/model/NodeTest.kt
@@ -0,0 +1,41 @@
+package com.develop.zuzik.fragmentnavigation.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotSame
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
+/**
+ * User: zuzik
+ * Date: 1/21/17
+ */
+class NodeTest {
+
+    @Test
+    fun nodeIsSerializable() {
+        val node =
+                Node<String>("a1", "a1Value", "b1", mutableListOf(
+                        Node<String>("b1", "b1Value", "c1", mutableListOf(
+                                Node<String>("c1", "c1Value", null, mutableListOf()),
+                                Node<String>("c2", "c2Value", null, mutableListOf())
+                        ))
+                ))
+
+        ByteArrayOutputStream().use { baos ->
+            ObjectOutputStream(baos).use { oos ->
+                oos.writeObject(node)
+                ByteArrayInputStream(baos.toByteArray()).use { bais ->
+                    ObjectInputStream(bais).use { ois ->
+                        val readNode = ois.readObject() as Node<String>
+                        assertNotSame(node, readNode)
+                        assertEquals(node, readNode)
+                    }
+                }
+            }
+        }
+    }
+
+}
